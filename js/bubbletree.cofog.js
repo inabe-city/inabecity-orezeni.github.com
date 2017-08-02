016804ebe2355af780a869ed2341836f67891d5a
Hideki Oride <oride@gachapom.jp>
2017-08-02 10:58:20 +0900

    アイコン修正

--- js/bubbletree.cofog.js
+++ js/bubbletree.cofog.js
@@ -14,13 +14,13 @@ BubbleTree.Styles.Cofog1 = {
         '03': { icon: 'icons/civilian-action.svg', color: '#EC2406' },
         '04': { icon: 'icons/environment.svg', color: '#790586' },
         '05': { icon: 'icons/farms.svg', color: '#2A3A03' },
-        '06': { icon: 'icons/garbage.svg', color: '#D33673' },
-        '07': { icon: 'icons/our-streets.svg', color: '#D33673' },
+        '06': { icon: 'icons/planning.svg', color: '#D33673' },
+        '07': { icon: 'icons/coal.svg', color: '#D33673' },
         '08': { icon: 'icons/fire-brigade.svg', color: '#4E6D00' },
-        '09': { icon: 'icons/secondary-upper.svg', color: '#C75746' },
+        '09': { icon: 'icons/schools.svg', color: '#C75746' },
         '10': { icon: 'icons/construction.svg', color: '#938626' },
         '11': { icon: 'icons/public-debt.svg', color: '#938626' },
-        '12': { icon: 'icons/traffic-watersup.svg', color: '#938626' },
+        '12': { icon: 'icons/japanese-money.svg', color: '#938626' },
         '13': { icon: 'icons/money.svg', color: '#938626' }
 }
 
@@ -33,10 +33,10 @@ BubbleTree.Styles.Cofog2 = {
         '04.1': { icon: 'icons/environment.svg'},
         '05.1': { icon: 'icons/farms.svg'},
         '05.2': { icon: 'icons/tree.svg'},
-        '06.1': { icon: 'icons/garbage.svg'},
-        '07.1': { icon: 'icons/housing.svg'},
-        '07.2': { icon: 'icons/street-lights.svg'},
-        '07.3': { icon: 'icons/farms.svg'},
+        '06.1': { icon: 'icons/planning.svg'},
+        '07.1': { icon: 'icons/coal.svg'},
+        '07.2': { icon: 'icons/coal.svg'},
+        '07.3': { icon: 'icons/coal.svg'},
         '08.1': { icon: 'icons/culture.svg'},
         '08.2': { icon: 'icons/dollar.svg'},                                                                                        
         '08.3': { icon: 'icons/harbor.svg'},
@@ -44,7 +44,7 @@ BubbleTree.Styles.Cofog2 = {
         '09.2': { icon: 'icons/schools.svg'},
         '10.1': { icon: 'icons/construction.svg'},
         '11.1': { icon: 'icons/public-debt.svg'},
-        '12.1': { icon: 'icons/traffic-watersup.svg'},
+        '12.1': { icon: 'icons/japanese-money.svg'},
         '13.1': { icon: 'icons/money.svg'}
 };
 
